2be3d18af892fa6cc2907e6478513abd5032876e
thanh-tung-25 <dangthanhtung113114115@gmail.com>
2025-07-24 16:19:23 +0700

    one

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarController;
 
-Route::resource('cars', CarController::class);
+Route::get('/', [CarController::class, 'index']);
+
 
 Route::get('/', function () {
     return view('welcome');
